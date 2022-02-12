dcbeda42fe68f730ef8a32b451100e4e40f63ac7
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2022-02-12 19:37:55 +0500

    Auth login register with bootstrap ui

---
+++ Auth-login-register-with-bootstrap.php
@@ -0,0 +1,18 @@
+
+
+1.Create New Project
+
+2.Create Database
+//After Creating database
+//go to .env file and connect application with Database
+
+//enter this on terminal
+3.composer require laravel/ui
+
+4.php artisan ui bootstrap --auth
+
+5.npm install
+
+6.npm run watch
+
+7.php artisan serve 
